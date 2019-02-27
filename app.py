3993f56cda0c4f21d97c1b349a70ad1665369f02
Corey Gillen <corey.j.gillen@gmail.com>
2019-02-27 09:16:17 -0800

    Update client to Asnake
    Add some startup error messaging

--- app.py
+++ app.py
@@ -1,42 +1,12 @@
 import configparser
 import argparse
-import aspace
+import time
 import logging
+import requests
+
+from asnake.client import ASnakeClient
 
 from tasks import *
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-y", "--yes", help="Supply Yes to all confirmation prompts", action="store_true")
-args = parser.parse_args()
-
-config = configparser.ConfigParser()
-config.read('settings.ini')
-
-client = (
-  aspace.client.ASpaceClient
-  .init_from_config(config)
-  .wait_until_ready(
-    check_interval=2.0,
-    max_wait_time=200.0,
-    authenticate_on_success=True,
-  )
-)
-
-logger = logging.getLogger('archivespace')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('app.log')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
 
 def print_menu(tasks):
   print("Choose a task:")
@@ -80,4 +50,43 @@ def main_menu():
 
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-y", "--yes", help="Supply Yes to all confirmation prompts", action="store_true")
+  args = parser.parse_args()
+
+  logger = logging.getLogger('archivespace')
+  logger.setLevel(logging.DEBUG)
+  # create file handler which logs even debug messages
+  fh = logging.FileHandler('app.log')
+  fh.setLevel(logging.DEBUG)
+  # create console handler with a higher log level
+  ch = logging.StreamHandler()
+  ch.setLevel(logging.INFO)
+  # create formatter and add it to the handlers
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  fh.setFormatter(formatter)
+  ch.setFormatter(formatter)
+  # add the handlers to the logger
+  logger.addHandler(fh)
+  logger.addHandler(ch)
+
+  config = configparser.ConfigParser()
+  config.read('settings.ini')
+
+  try:
+    client = ASnakeClient(baseurl=config['aspace_credentials']['api_host'],
+                          username=config['aspace_credentials']['username'],
+                          password=config['aspace_credentials']['password'])
+  except KeyError as e:
+    logger.error('settings.ini does not exist or is invalid')
+    raise e
+
+  # Simple sanity check to make sure client is setup
+  try:
+    resp = client.get('/')
+    if not resp.ok:
+      resp.raise_for_status()
+  finally:
+    logger.error('Unable to contact ArchivesSpace instance')
+
   main_menu()
